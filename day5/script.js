ad30bf21af2556e01bf88b4767a426f87c727cbb
PRERNAVATS <vatsprerna10@gmail.com>
2024-10-14 10:34:42 +0530

    day5

---
+++ day5/script.js
@@ -0,0 +1,57 @@
+// function display(){
+//     // alert("HELLO JS");
+//     console.log("HELLO JS");
+// }
+// display();
+
+// //check the type of variable
+// let x = 12;
+// let y = "HII";
+// console.log(typeof x);
+// console.log(typeof y);
+
+// // object
+// let obj = {
+//     'name':"PRERNA",
+//     id:12,
+// }
+// console.log(typeof obj);
+
+// // array-also an object
+// let arr=[1,2,34,5];
+// console.log(typeof arr);
+
+// //undefined 
+// let a;
+// console.log(typeof a);
+
+// function validate(){
+//  let un = document.getElementById('un').value;   
+//  let pd = document.getElementById('pw').value;   
+// //  alert('un');
+// if(un == 'admin' && pd == 'admin')
+//     document.getElementById('res').innerHTML="LOGIN SUCCESS";
+// else
+// document.getElementById('res').innerHTML="LOGIN FAIL";
+// }
+
+// let X = Number("12");
+// let str = Number("123");
+// let b = Boolean(12);
+// console.log(b);
+
+
+  // arrow function
+//   let a = (x,y)=>{
+//     console.log(x+y);
+// };
+// a(12,34);
+
+
+// another function
+ function display(){
+    let p1 = document.querrySelector("p");
+    p1.innerHTML = "RESULT";
+    p1.style.backgroundColor = "red";
+ }
+
